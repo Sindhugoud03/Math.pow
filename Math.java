c880edfe9caba34c84072725d5fa6cb89173077d
Sindhugoud03 <sindhugoudlingaram@gmail.com>
2025-01-25 14:32:25 +0530

    Create Math.java

---
+++ Math.java
@@ -0,0 +1,13 @@
+Public class Main {
+  public static void main(String[] args) {
+  Scanner sc = new scanner (System.in);
+  System.out.print("Enetre the base number:);
+  int base=sc . nextInt();
+  System.out.print("Enter the Exponent:");
+  int exponent= sc .nextInt();
+    double result=Math.pow(base,exponent);
+    System.out.println("Result");
+    sc.close();
+  }
+}
+    
