b00917c7e6de267b9e1db520b2b6c9ce336057b5
Sindhugoud03 <sindhugoudlingaram@gmail.com>
2025-01-28 09:53:00 +0530

    Delete Operators.java

--- Operators.java
+++
@@ -1,8 +0,0 @@
-public class Operator {
-    public static void main(String[] args) {
-        int a =10, b = 5;
-        System.out.println("Addition:"+(a+b));
-        System.out.println("Relational"+(a=b));
-        System.out.println("Logical AND:"+(a>5 && b>2));
-    }
-}
